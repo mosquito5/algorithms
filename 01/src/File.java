2ca156b52c998db1de3a5af9565df97d312f1ac8
mosquito <jakub.moskal3@gmail.com>
2017-12-07 20:33:43 +0100

    generated files on 2017-12-07 20:33:43

---
+++ 01/src/File.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class File {
+    public String[] readToArray() {
+        List<String> lines = new ArrayList<String>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("fi"));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            bufferedReader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines.toArray(new String[lines.size()]);
+    }
+}
+
---
+++ 01/src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String args[]) {
+        File file = new File();
+        Change change = new Change(file.readToArray());
+        change.start();
+        //change.arrlenght();
+    }
+}
