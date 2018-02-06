b5d1cba717ce24b6143a31ef50330d6b7da03a63
mosquito <jakub.moskal3@gmail.com>
2018-02-06 22:25:49 +0100

    generated files on 2018-02-06 22:25:49

---
+++ src/chapter3/BST/BST.java
@@ -0,0 +1,32 @@
+package chapter3.BST;
+
+
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        private int N;
+
+        public Node(Key key, Value val, int N) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
+
+        public int size() {
+            return size(root);
+        }
+
+        private int size(Node x) {
+            if(x == null)
+                return 0;
+            else
+                return x.N;
+        }
+
+    }
+
+}
---
+++ src/chapter3/BST/Main.java
@@ -0,0 +1,4 @@
+package chapter3.BST;
+
+public class Main {
+}
